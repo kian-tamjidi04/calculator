7a6e5cc614beba46bea4ce5a824d7493dbab8ba5
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2024-09-02 18:08:47 +0100

    Change calc functionality so now works

--- functions.js
+++ functions.js
@@ -35,39 +35,28 @@ function operate(operator, x, y) {
 
 const numContainer = document.querySelector(".numbers");
 const display = document.querySelector(".display");
-let displayVal = display.textContent; // Remember that this value is a string for now
 
 numContainer.addEventListener('click', (event) => {
     // Prevents leading zeros and the = symbol displaying
     if (event.target.value === "=") {
-        displayVal += "";
-    } else if (displayVal == 0 || displayVal == "") {
-        displayVal = event.target.value;
+        display.textContent += "";
+    } else if (display.textContent == 0 || display.textContent == "") {
+        display.textContent = event.target.value;
     } else {
-        displayVal += event.target.value;
+        display.textContent += event.target.value;
     }
-    display.textContent = displayVal;
 });
-
-// Make the calculator work:
-    // Take one value
-    // Store operator pressed
-    // Take second value
-    // Wait until = is pressed
-    // Change output on display to be the result of the calculation
 
 const operationContainer = document.querySelector(".operations");
 operationContainer.addEventListener('click', (event) => {
-    firstVal = parseInt(displayVal);
+    firstVal = parseInt(display.textContent);
     currentOperation = event.target.value;
-    displayVal = "";
-    display.textContent = displayVal;
+    display.textContent = "";
 })
 
 const equalsBtn = document.querySelector(".equals-button");
 equalsBtn.addEventListener('click', () => {
-    secondVal = parseInt(displayVal);
-    displayVal = "";
-    display.textContent = `${operate(currentOperation, firstVal, secondVal)}`;
-    console.log(operate(currentOperation, firstVal, secondVal))
+    secondVal = parseInt(display.textContent);
+    let res = operate(currentOperation, firstVal, secondVal);
+    display.textContent = res;
 });
