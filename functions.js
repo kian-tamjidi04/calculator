eab8515b67d3031572ee474684c9ea50bbe6fac5
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2024-09-02 16:06:28 +0100

    Initial attempt at calculator logic

--- functions.js
+++ functions.js
@@ -17,7 +17,7 @@ function divide(x, y) {
 // Typical operation: 3 + 5
 // firstVal = 3, operation = '+', secondVal = 5
 let firstVal;
-let operation;
+let currentOperation;
 let secondVal;
 
 function operate(operator, x, y) {
@@ -33,11 +33,41 @@ function operate(operator, x, y) {
     }
 }
 
-const container = document.querySelector(".numbers");
+const numContainer = document.querySelector(".numbers");
 const display = document.querySelector(".display");
-let displayVal = display.textContent;
+let displayVal = display.textContent; // Remember that this value is a string for now
 
-container.addEventListener('click', (event) => {
-    displayVal += event.target.value;
+numContainer.addEventListener('click', (event) => {
+    // Prevents leading zeros and the = symbol displaying
+    if (event.target.value === "=") {
+        displayVal += "";
+    } else if (displayVal == 0 || displayVal == "") {
+        displayVal = event.target.value;
+    } else {
+        displayVal += event.target.value;
+    }
     display.textContent = displayVal;
+});
+
+// Make the calculator work:
+    // Take one value
+    // Store operator pressed
+    // Take second value
+    // Wait until = is pressed
+    // Change output on display to be the result of the calculation
+
+const operationContainer = document.querySelector(".operations");
+operationContainer.addEventListener('click', (event) => {
+    firstVal = parseInt(displayVal);
+    currentOperation = event.target.value;
+    displayVal = "";
+    display.textContent = displayVal;
+})
+
+const equalsBtn = document.querySelector(".equals-button");
+equalsBtn.addEventListener('click', () => {
+    secondVal = parseInt(displayVal);
+    displayVal = "";
+    display.textContent = `${operate(currentOperation, firstVal, secondVal)}`;
+    console.log(operate(currentOperation, firstVal, secondVal))
 });
